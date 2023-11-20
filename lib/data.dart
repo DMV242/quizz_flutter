ccb1d5abbf47c03cb5926ffbfff3abf9001a1280
MMEDV242 <mvouladavid@gmail.com>
2023-11-20 11:23:27 +0100

    add question

--- lib/data.dart
+++ lib/data.dart
@@ -3,6 +3,7 @@ import 'question.dart';
 class Datas {
 
   List<Question> listeQuestions = [
+    Question(question: 'Lebron James est meilleur que Michael Jordan', reponse: true, explication: 'C\'est réel' , imagePath: 'lebronvsJordan.png'),
     Question(question: 'La devise de la Belgique est l\'union fait la force', reponse: true, explication: '', imagePath: 'belgique.jpg'),
     Question(question: 'La lune va finir par tomber sur terre à cause de la gravité', reponse: false, explication: 'Au contraire la lune s\'éloigne', imagePath: 'lune.jpg'),
     Question(question: 'La Russie est plus grande en superficie que Pluton', reponse: true, explication: '', imagePath: 'russie.jpg'),
@@ -13,6 +14,7 @@ class Datas {
     Question(question: 'La barbe des pharaons était fausse', reponse: true, explication: 'A l\'époque déjà ils utilisaient des postiches', imagePath: 'pharaon.jpg'),
     Question(question: 'Au Québec tire toi une bûche veut dire viens viens t\'asseoir', reponse: true, explication: 'La bûche, fameuse chaise de bucheron', imagePath: 'buche.jpg'),
     Question(question: 'Le module lunaire Eagle de possédait de 4Ko de Ram', reponse: true, explication: 'Dire que je me plains avec mes 8GO de ram sur mon mac', imagePath: 'eagle.jpg'),
+
   ];
 
 }
